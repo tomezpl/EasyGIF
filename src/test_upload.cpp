69c8a613f3d87e2225aea7e08e08163472e8283f
KingCprey <thatdevbloke@gmail.com>
2016-09-05 10:20:42 +0100

    Back to Mehiko w/ Juan

--- src/test_upload.cpp
+++ src/test_upload.cpp
@@ -1,17 +1,24 @@
 #include "net/gyazo.hpp"
 #include <iostream>
 #include <string>
+using namespace std;
 int main(int argc,char *argv[]){
 	//std::cout<<"IT WORKED AHMAGAWD"<<std::endl;
-	EasyGIF::Uploaders::Gyazo gyazo;
-	std::cout<<gyazo.GetUserAgent()<<std::endl;
-	struct EasyGIF::Uploaders::ImageUpload to_upload;
-	to_upload.proxy_settings="http://localhost:8000";
-	to_upload.file_path="dc.png";
-	to_upload.file_name="davidcameronontoast";
-	struct EasyGIF::Uploaders::CompletedImageUpload up;
-	up=gyazo.uploadFile(to_upload);
-	std::cout<<up.successful<<std::endl;
-	std::cout<<up.hex_digest<<std::endl;
+	EasyGIF::Uploaders::GyazoCompletedUpload ciu;
+	cout<<"Uploading dc.png to Gyazo"<<endl;
+	EasyGIF::Uploaders::UploadFileToGyazo(&ciu,"dc.png",true);
+	cout<<"Upload process complete"<<endl;
+	cout<<"Upload URL = "<<ciu.upload_url<<endl;
+	cout<<"Upload file = "<<ciu.upload_file<<endl;
+	cout<<"Upload filename = "<<ciu.upload_filename<<endl;
+	cout<<"Upload GID = "<<ciu.gyazo_id<<endl;
+	cout<<"Upload User Agent = "<<ciu.user_agent<<endl;
+	cout<<"Upload proxy settings = "<<ciu.proxy_settings<<endl;
+	cout<<"Upload MD5 Digest = "<<ciu.md5_digest<<endl;
+	cout<<"Upload Successful = "<<ciu.curl_successful<<endl;
+	if(!ciu.curl_successful){
+		cout<<"CURL strerror"<<endl;
+		curl_easy_strerror(ciu.curl_response);
+	}
 	return 0;
 }
