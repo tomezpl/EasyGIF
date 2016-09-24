0337e23d3479c9cd8db9f568a4fabae5bd7a0297
Tomasz Zając <tomezpl@gmail.com>
2016-09-24 19:36:31 +0100

    Refactored ImageSaver to be based on the ImageContainer class

--- src/img/image_saver.cpp
+++ src/img/image_saver.cpp
@@ -5,40 +5,11 @@
 
 using namespace EasyGIF;
 
-void ImageSaver::LoadData(ImageFrame frame)
-{
-	m_Image.create(frame.GetWidth(), frame.GetHeight(), sf::Color::White);
-
-	//if(data.size() == 0)
-	//{
-		//std::cout << "Wrong pixel data" << std::endl;
-		//return;
-	//}
-	uint8_t* buf = frame.GetBuffer();
-
-	for(unsigned long y = 0; y < frame.GetHeight(); y++)
-	{
-		unsigned long realX = 0;
-		for(unsigned long x = y * 4 * frame.GetWidth(); x < (y + 1) * frame.GetWidth() * 4; x += 4)
-		{
-			uint8_t currR = buf[x];
-			uint8_t currG = buf[x + 1];
-			uint8_t currB = buf[x + 2];
-			m_Image.setPixel(realX, y, sf::Color((int)(currR), (int)(currG), (int)(currB), 255));
-
-			/*std::cout << "Pixel (" << x+1 << ", " << y+1 << ") has colour (" 
-				  << (int)(m_Image.getPixel(x, y).r) << ", "
-				  << (int)(m_Image.getPixel(x, y).g) << ", "
-				  << (int)(m_Image.getPixel(x, y).b) << ", "
-				  << (int)(m_Image.getPixel(x, y).a) << ")"
-				  << std::endl;*/
-			realX++;
-		}
-	}
-}
-
 bool ImageSaver::Save(std::string fileName)
 {
+	#ifdef EZGIF_DEBUG
+	std::cout << "Filename is " << fileName;
+	#endif
 	if(m_Image.saveToFile(fileName))
 	{
 		std::cout << "File saved successfully" << std::endl;
--- src/img/image_saver.hpp
+++ src/img/image_saver.hpp
@@ -1,27 +1,17 @@
 // image_saver.hpp
 // Author: Tomasz Zajac
 // The ImageSaver class provides an interface to save an image (e.g. one captured with ImageGrabber) to a file using SFML.
+// It is based on the ImageContainer class.
 
 #ifndef EZGIF_IMAGE_SAVER
 #define EZGIF_IMAGE_SAVER
 
-#include <SFML/Graphics/Image.hpp>
-#include <iostream>
-#include <vector>
-#include "image_frame.hpp"
+#include "image_container.hpp"
 
 namespace EasyGIF {
-	class ImageSaver {
-	private:
-		sf::Image m_Image; // Image object that will be used to save the image to a file
+	class ImageSaver : public ImageContainer {
 	public:
-		// This method loads pixels from the ImageFrame into the image object.
-		// The ImageFrame will contain a vector of vectors of pixels,
-		// returned from ImageGrabber::Grab(), for example.
-		// Although it was previously possible to only pass a vector of vectors of pixels,
-		// this is a more "elegant" way of doing it.
-		void LoadData(ImageFrame frame);
-
+		
 		// This function saves the previously constructed image (by using LoadData())
 		// to a file, and returns true on success, and false on error.
 		bool Save(std::string fileName);
