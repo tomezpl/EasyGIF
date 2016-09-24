2380c51bb0ca4d5089d99f1351be8ca1e6c86d23
Tomasz Zając <tomezpl@gmail.com>
2016-09-24 19:34:58 +0100

    Added the RegionPicker class

---
+++ src/ui/region_picker.cpp
@@ -0,0 +1,140 @@
+// region_picker.cpp
+// Author: Tomasz Zajac
+
+#include "region_picker.hpp"
+
+using namespace EasyGIF::UI;
+
+RegionPicker::RegionPicker(bool activate)
+{
+	m_IsPicking = false;
+	m_Region = {0, 0, 0, 0};
+	m_XInverted = false;
+	m_YInverted = false;
+
+	if(activate)
+		this->Activate();
+}
+
+void RegionPicker::getDesktopSprite()
+{
+	::EasyGIF::ImageFrame desktopScreenshot(m_Grabber.Grab());
+	m_WinWidth = desktopScreenshot.GetWidth();
+	m_WinHeight = desktopScreenshot.GetHeight();
+	m_DesktopTexture.create(m_WinWidth, m_WinHeight);
+	m_DesktopTexture.loadFromImage(m_Converter.ImgFrameToSFImage(desktopScreenshot));
+	m_Desktop.setTexture(m_DesktopTexture);
+	m_Desktop.setPosition(0, 0);
+}
+
+void RegionPicker::Activate()
+{
+	this->getDesktopSprite();
+	#ifdef EZGIF_DEBUG
+	std::cout << "RegionPicker " << std::to_string(m_WinWidth) << "x" << std::to_string(m_WinHeight) << std::endl;
+	#endif
+	m_Window.create(sf::VideoMode(m_WinWidth, m_WinHeight), "EasyGIF RegionPicker", sf::Style::Fullscreen);
+	m_IsRunning = true;
+}
+
+bool RegionPicker::IsRunning()
+{
+	return m_IsRunning;
+}
+
+void RegionPicker::ProcessInput()
+{
+	bool isPicking = sf::Mouse::isButtonPressed(sf::Mouse::Left);
+
+	sf::Vector2i mousePos = sf::Mouse::getPosition();
+
+	if(isPicking)
+	{
+		if(m_IsPicking)
+		{
+			if(m_Region.x > mousePos.x)
+				m_XInverted = true;
+			else
+				m_XInverted = false;
+
+			if(m_Region.y > mousePos.y)
+				m_YInverted = true;
+			else
+				m_YInverted = false;
+
+			m_Region.width = mousePos.x - m_Region.x;
+			m_Region.height = mousePos.y - m_Region.y;
+		}
+		else
+		{
+			m_Region.x = mousePos.x;
+			m_Region.y = mousePos.y;
+			m_Region.width = 0;
+			m_Region.height = 0;
+		}
+	}
+
+	m_IsPicking = isPicking;
+}
+
+void RegionPicker::UpdateEvents()
+{
+	if(m_Window.isOpen())
+	{
+		sf::Event event;
+		while(m_Window.pollEvent(event))
+		{
+			if(event.type == sf::Event::Closed)
+			{
+				m_Window.close();
+				m_IsRunning = false;
+			}
+		}
+	}
+}
+
+void RegionPicker::Draw()
+{
+	if(m_Window.isOpen())
+	{
+		sf::Texture selectionTexture;
+		sf::Image selectionTextureImg;
+		sf::Sprite selection;
+
+		if(m_Region.width != 0 && m_Region.height != 0 && m_Region.width < m_WinWidth && m_Region.height < m_WinHeight)
+		{
+			selectionTextureImg.create(std::abs(m_Region.width), std::abs(m_Region.height));
+			selectionTexture.create(std::abs(m_Region.width), std::abs(m_Region.height));
+
+			for(unsigned short x = 0; x < std::abs(m_Region.width); x++)
+			{
+				for(unsigned short y = 0; y < std::abs(m_Region.height); y++)
+					selectionTextureImg.setPixel(x, y, sf::Color::White);
+			}
+
+			selectionTexture.loadFromImage(selectionTextureImg);
+
+			selection.setTexture(selectionTexture);
+			selection.setColor(sf::Color(64, 64, 64, 64));
+
+			unsigned short xPos = 0, yPos = 0;
+			if(m_XInverted)
+				xPos = m_Region.x + m_Region.width;
+			else
+				xPos = m_Region.x;
+
+			if(m_YInverted)
+				yPos = m_Region.y + m_Region.height;
+			else
+				yPos = m_Region.y;
+
+			selection.setPosition(xPos, yPos);
+		}
+
+		m_Window.clear();
+		m_Window.draw(m_Desktop);
+		if(m_Region.width != 0 && m_Region.height != 0 && m_Region.width < m_WinWidth && m_Region.height < m_WinHeight)
+			m_Window.draw(selection);
+		m_Window.display();
+	}
+}
---
+++ src/ui/region_picker.hpp
@@ -0,0 +1,57 @@
+// region_picker.hpp
+// Author: Tomasz Zajac
+// The RegionPicker class provides a UI component that lets the user select the region for the screen capture.
+
+#ifndef EZGIF_UI_REGIONPICKER
+#define EZGIF_UI_REGIONPICKER
+
+#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+#include "../img/image_container.hpp"
+#include "../img/image_frame.hpp"
+#include "../img/image_grabber.hpp"
+#include "../util/converter.hpp"
+#include "../util/xhelper.hpp"
+#include <string>
+#include <cmath>
+
+namespace EasyGIF {
+	namespace UI {
+		class RegionPicker : public ImageContainer {
+		private:
+			bool m_IsPicking; // is the user picking the region? (ie. is left mouse button pressed)
+			::EasyGIF::Utility::Rectangle m_Region; // selected region
+			sf::RenderWindow m_Window; // Window to draw UI
+			::EasyGIF::ImageGrabber m_Grabber; // desktop grabber
+			sf::Sprite m_Desktop; // desktop sprite to draw in the UI
+			sf::Texture m_DesktopTexture; // texture for the desktop sprite
+			void getDesktopSprite(); // initialise the desktop sprite
+			unsigned short m_WinWidth, m_WinHeight; // window size
+			::EasyGIF::Utility::Converter m_Converter; // utility converter class
+			bool m_IsRunning; // is the region picker running?
+			bool m_XInverted, m_YInverted; // is the selection "inverted"?
+		public:
+			// Default constructor
+			// Also activates/initialises/launches (you name it) the region picker
+			// (unless activate is set to false, of course)
+			RegionPicker(bool activate = true);
+
+			// Creates and initialises the UI, launches the region picker
+			void Activate();
+
+			// Returns whether the region picker is running or not
+			bool IsRunning();
+
+			// Process mouse input in order to update the region rectangle
+			void ProcessInput();
+
+			// Update window events
+			void UpdateEvents();
+
+			// Draw the UI
+			void Draw();
+		};
+	}
+}
+
+#endif
