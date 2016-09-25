8ae61fdd231dec389704781e9abf69398b521578
Tomasz Zając <tomezpl@gmail.com>
2016-09-25 14:55:45 +0100

    Test for saving a selected region to a file

--- src/test_picker.cpp
+++ src/test_picker.cpp
@@ -1,14 +1,33 @@
+#include "img/image_frame.hpp"
+#include "img/image_grabber.hpp"
+#include "img/image_saver.hpp"
 #include "ui/region_picker.hpp"
+#include <unistd.h>
 
 int main()
 {
 	EasyGIF::UI::RegionPicker picker(false);
+	EasyGIF::ImageFrame frame;
+	EasyGIF::ImageGrabber grabber;
+	EasyGIF::ImageSaver saver;
 	picker.Activate();
 	while(picker.IsRunning())
 	{
 		picker.UpdateEvents();
-		picker.ProcessInput();
-		picker.Draw();
+		if(picker.IsConfirmed())
+		{
+			::EasyGIF::Utility::Rectangle rect = picker.GetRect();
+			picker.Shutdown();
+			sleep(5); // let X close the window
+			frame = grabber.Grab(rect);
+			saver.LoadData(frame);
+			saver.Save("./test_picker.png");
+		}
+		else
+		{
+			picker.ProcessInput();
+			picker.Draw();
+		}
 	}
 
 	return 0;
