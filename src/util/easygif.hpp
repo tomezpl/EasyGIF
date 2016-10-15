e957b89784a530b39b98ae68b5b9d9544a079fb7
Tomasz Zając <tomezpl@gmail.com>
2016-10-15 19:46:25 +0100

    Added description for the EasyGIF namespace

---
+++ src/util/easygif.hpp
@@ -0,0 +1,7 @@
+// DO NOT COMPILE THIS
+// This is only going to be parsed by Doxygen to get the descriptions for the EasyGIF namespace, nothing else.
+
+/**
+* Global namespace for the EasyGIF program's modules.
+*/
+namespace EasyGIF {}
