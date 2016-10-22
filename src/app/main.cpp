87ed94ac9a47bb17d78d0f58eefc210aadafd929
Tomasz Zając <tomezpl@gmail.com>
2016-10-22 16:21:20 +0100

    Updated UI with a "Capture" option

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -4,57 +4,16 @@
 #include <iostream>
 #include <string>
 #include <gtk/gtk.h>
-
-void TakeScreenshot(std::string);
-void RecordGIF(std::string);
+#include <unistd.h>
+#include <xcb/xcb.h>
 
 const std::string g_defaultModule = "screenshot_static";
 
 std::string g_filename = "";
 std::string g_module = "";
 
-int main(int argc, char** argv)
-{
-	GtkBuilder* builder;
-	GtkWidget* window;
-	GError* error = nullptr;
-
-	gtk_init(&argc, &argv);
-
-	builder = gtk_builder_new();
-	if(!gtk_builder_add_from_file(builder, EasyGIF::FileSystem::GetAssetPath("./ui/ezapp.glade"), &error))
-	{
-		g_warning("%s", error->message);
-		g_free(error);
-		return 1;
-	}
-
-	window = GTK_WIDGET(gtk_builder_get_object(builder, "mainWnd"));
-	gtk_builder_connect_signals(builder, nullptr);
-
-	g_object_unref(G_OBJECT(builder));
-
-	// check if user wants to launch a specific module (e.g. screenshot_gif) right away
-	if(argc > 1)
-		g_module = argv[1];
-
-	if(argc > 2)
-		g_filename = argv[2];
-
-	if(g_module == "screenshot_static")
-		TakeScreenshot(g_filename);
-	else if(g_module == "screenshot_gif")
-		RecordGIF(g_filename);
-	else if(g_module != "")
-		std::cout << "Unknown module \"" << g_module << std::endl;
-	else if(g_module == "")
-	{
-		gtk_widget_show(window);
-		gtk_main();
-	}
-
-	return 0;
-}
+GtkBuilder* g_pBuilder = nullptr;
+GtkWidget* g_pMainWnd = nullptr;
 
 void TakeScreenshot(std::string filename = "")
 {
@@ -76,4 +35,79 @@ void RecordGIF(std::string filename = "")
 		filename = "./screenshot_gif.gif";
 	
 	sg.Save(filename);
+}
+
+extern "C" {
+	void ui_btn_capture_gif(GtkButton* widget, gpointer user_data)
+	{
+		GtkWidget* pCaptureWnd = gtk_widget_get_toplevel(GTK_WIDGET(widget));
+		gtk_widget_destroy(pCaptureWnd);
+		RecordGIF(g_filename);
+		gtk_main_quit();
+	}
+
+	void ui_btn_capture_ss(GtkButton* widget, gpointer user_data)
+	{
+		GtkWidget* pCaptureWnd = gtk_widget_get_toplevel(GTK_WIDGET(widget));
+		gtk_widget_destroy(pCaptureWnd);
+		TakeScreenshot(g_filename);
+		gtk_main_quit();
+	}
+
+	void ui_btn_capture(GtkButton* widget, gpointer user_data)
+	{
+		std::cout << "You clicked \"Capture\"" << std::endl;
+		GtkWidget* pCaptureWnd = GTK_WIDGET(gtk_builder_get_object(g_pBuilder, "captureDlgWnd"));
+		std::cout << "captureDlgWnd grabbed" << std::endl;
+		gtk_widget_show(pCaptureWnd);
+		std::cout << "captureDlgWnd shown" << std::endl;
+		gtk_widget_destroy(g_pMainWnd);
+		std::cout << "mainWnd destroyed" << std::endl;
+	}
+}
+
+int main(int argc, char** argv)
+{
+	GError* error = nullptr;
+
+	gtk_init(&argc, &argv);
+
+	g_pBuilder = gtk_builder_new();
+	if(!gtk_builder_add_from_file(g_pBuilder, EasyGIF::FileSystem::GetAssetPath("./ui/ezapp.glade"), &error))
+	{
+		g_warning("%s", error->message);
+		g_free(error);
+		return 1;
+	}
+
+	gtk_builder_connect_signals(g_pBuilder, nullptr);
+
+	gtk_builder_add_callback_symbol(g_pBuilder, "ui_btn_capture", G_CALLBACK(ui_btn_capture));
+	gtk_builder_add_callback_symbol(g_pBuilder, "ui_btn_capture_gif", G_CALLBACK(ui_btn_capture_gif));
+	gtk_builder_add_callback_symbol(g_pBuilder, "ui_btn_capture_ss", G_CALLBACK(ui_btn_capture_ss));
+
+	g_pMainWnd = GTK_WIDGET(gtk_builder_get_object(g_pBuilder, "mainWnd"));
+
+	// check if user wants to launch a specific module (e.g. screenshot_gif) right away
+	if(argc > 1)
+		g_module = argv[1];
+
+	if(argc > 2)
+		g_filename = argv[2];
+
+	if(g_module == "screenshot_static")
+		TakeScreenshot(g_filename);
+	else if(g_module == "screenshot_gif")
+		RecordGIF(g_filename);
+	else if(g_module != "")
+		std::cout << "Unknown module \"" << g_module << std::endl;
+	else if(g_module == "")
+	{
+		gtk_widget_show(g_pMainWnd);
+		gtk_main();
+	}
+
+	g_object_unref(G_OBJECT(g_pBuilder));
+
+	return 0;
 }
